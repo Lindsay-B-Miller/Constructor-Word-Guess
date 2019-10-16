680bb7be5fbe0ca2d784a263c753843ff8a8a17c
Lindsay <lindsay.lbm@gmail.com>
2019-10-16 09:07:26 -0500

    added inquirer prompt in index

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 var inquirer = require("inquirer");
 
-
+function inquirerLetterGuess() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "letter",
+            message: "What is your letter selection?"
+        }
+    ]).then(function (userGuess) {
+        console.log(userGuess.letter);
+    })
+}
+inquirerLetterGuess()
 
